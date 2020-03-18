2973e5615c517a73517d27d904751fd2677c1a28
анастасия <pristupa1998@mail.ru>
2020-03-18 19:52:11 +0000

    finish

--- 1.sql
+++ 1.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE mriley;
+USE mriley;
 CREATE TABLE koronvirus (
     name    char(20) NOT NULL,
     firstname  char(30) NOT NULL,
     virus       tinyint NOT NULL,
-    id          int     NOT NULL.
+    id          int     NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -13,7 +15,7 @@ CREATE TABLE vich (
     name    char(20) NOT NULL,
     firstname  char(30) NOT NULL,
     virus       tinyint NOT NULL,
-    id          int     NOT NULL.
+    id          int     NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -24,9 +26,14 @@ CREATE TABLE grip (
     name    char(20) NOT NULL,
     firstname  char(30) NOT NULL,
     virus       tinyint NOT NULL,
-    id          int     NOT NULL.
+    id          int     NOT NULL,
     PRIMARY KEY (id)
 );
 
 INSERT INTO grip VALUES
 ('Anastasia','Ivanova',1,1),('Kiril','Khmel',1,2),('Sergei','Varenickov',0,3),('Nikita','Lian',0,4);
+SELECT vich.firstname FROM vich INNER JOIN koronvirus ON vich.firstname =koronvirus.firstname;
+SELECT *FROM grip AS t1 JOIN vich AS t2 LEFT JOIN koronvirus AS t3 ON t1.virus = t2.virus AND t2.virus = t3.virus;
+SELECT *FROM grip AS t1 JOIN vich AS t2 RIGHT JOIN koronvirus AS t3 ON t1.firstname = t2.firstname AND t2.firstname = t3.firstname;
+SELECT *FROM grip AS t1 JOIN vich AS t2 RIGHT JOIN koronvirus AS t3 ON t1.firstname = t2.firstname OR t1.firstname = t3.firstname WHERE t1.virus = 1 OR t2.virus = 1 OR t3.virus = 1;
+
